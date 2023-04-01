b531033213b46f978d9a60b13cb3445964961efb
Savelios <avel_stefan@outlook.com>
2023-04-01 15:46:03 +0300

    add users-info-container

--- src/main/java/com/example/demo/backend/views/HeaderView.java
+++ src/main/java/com/example/demo/backend/views/HeaderView.java
@@ -6,26 +6,16 @@ import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
-
 public class HeaderView extends HorizontalLayout {
-
-//    private final Div btnContainer = new Div();
-//    private final Button usersBtn = new Button();
-//    private final Button carsBtn = new Button();
-//    private final Button tracksBtn = new Button();
     private final AuthenticatedUser authenticatedUser;
-
     public HeaderView(AuthenticatedUser authenticatedUser) {
         this.authenticatedUser = authenticatedUser;
         screen();
     }
-
     public void screen() {
         Button authButton = new Button("Вход  систему");
         authButton.addClassNames("auth-button");
-        authButton.addClickListener(event -> {
-            authButton.getUI().ifPresent(ui -> ui.navigate("/auth"));
-        });
+
 
         Image brLine = new Image();
         brLine.setSrc("https://i.ibb.co/7JGwZpX/line.png");
@@ -36,27 +26,25 @@ public class HeaderView extends HorizontalLayout {
         Button tracksBtn = new Button("Треки");
         btnsContainer.add(brLine, usersBtn, carsBtn, tracksBtn);
 
-        btnsContainer.addClassNames("btnsContainer");
+        btnsContainer.addClassNames("btnsContainer", "hidden");
         usersBtn.addClassNames("usersBtn");
         carsBtn.addClassNames("carsBtn");
         tracksBtn.addClassNames("tracksBtn");
 
-//        if (authenticatedUser.get().isPresent()) {
-//            mapView.btnBuildLocation.setVisible(true);
-//            mapView.btnCenter.setVisible(true);
-//            mapView.btnLunch.setVisible(true);
-//            authButton.setVisible(false);
-//        }else {
-//            mapView.btnBuildLocation.setVisible(false);
-//            mapView.btnCenter.setVisible(false);
-//            mapView.btnLunch.setVisible(false);
-//            authButton.setVisible(true);
-//        }
+        authButton.addClickListener(event -> {
+            authButton.getUI().ifPresent(ui -> ui.navigate("/auth"));
+        });
+
+
+        if (!authenticatedUser.get().isPresent()) {
+            btnsContainer.setVisible(true);
+        } else {
+            btnsContainer.setVisible(false);
+        }
 
         this.addClassNames("view-header");
         this.add(createLogo(), btnsContainer, authButton);
     }
-
     public Div createLogo() {
         Div container = new Div();
         Anchor refresh = new Anchor("/", new Image("https://i.ibb.co/c112R87/gps-LOGOBlue.png", "My Alt Image"));
--- src/main/java/com/example/demo/backend/views/SideBarView.java
+++ src/main/java/com/example/demo/backend/views/SideBarView.java
@@ -6,6 +6,8 @@ import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.html.Anchor;
 import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.listbox.ListBox;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.textfield.TextArea;
 import com.vaadin.flow.data.value.ValueChangeMode;
@@ -16,10 +18,9 @@ import jakarta.annotation.security.RolesAllowed;
 import java.util.Arrays;
 
 public class SideBarView extends HorizontalLayout {
-
-//    private final Div trackContainer;
-//    private final Div carContainer;
-//    private final Div userContainer;
+    public  Div usersInfoContainer;
+    public  Div carsInfoContainer;
+    public Div tracksInfoContainer;
     public SideBarView() {
         screen();
     }
@@ -28,15 +29,44 @@ public class SideBarView extends HorizontalLayout {
         showButton.addClickListener(event -> {
 //            enterButton.getUI().ifPresent(ui -> ui.navigate("/"));
         });
-        showButton.addClassNames("show-button");
-
         Button createButton = new Button("Создать");
         createButton.addClickListener(event -> {
 //            enterButton.getUI().ifPresent(ui -> ui.navigate("/"));
         });
+        showButton.addClassNames("show-button");
         createButton.addClassNames("create-button");
 
-        addClassNames("sideBar");
-        add(showButton, createButton);
+        usersInfoContainer = new Div();
+        Label nameLabel = new Label("Имя");
+        Label showNameLabel = new Label("getName");
+        Label surnameLabel = new Label("Фамилия");
+        Label showSurnameLabel = new Label("GetLastname");
+        Label carLabel = new Label("Транспорт");
+        Label showCarLabel = new Label("GetCar");
+        Label registrationNumberLabel = new Label("ГОС-НОМЕР");
+        Label showRegistrationNumberLabel = new Label("GetRegistrationNumber");
+
+        nameLabel.addClassNames("nameLabel", "j");
+        showNameLabel.addClassNames("showNameLabel");
+        surnameLabel.addClassNames("surnameLabel", "j");
+        showSurnameLabel.addClassNames("showSurnameLabel");
+        carLabel.addClassNames("carLabel", "j");
+        showCarLabel.addClassNames("showCarLabel");
+        registrationNumberLabel.addClassNames("registrationNumberLabel", "j");
+        showRegistrationNumberLabel.addClassNames("showRegistrationNumberLabel");
+
+        usersInfoContainer.addClassNames("usersInfoContainer");
+        usersInfoContainer.add(
+                nameLabel,showNameLabel,
+                surnameLabel,showSurnameLabel,
+                carLabel,showCarLabel,
+                registrationNumberLabel,showRegistrationNumberLabel);
+
+        ListBox usersList = new ListBox();
+        usersList.addClassNames("usersList");
+        usersList.add(usersInfoContainer);
+
+        this.addClassNames("sideBar");
+        this.add(showButton, createButton, usersList);
     }
 }
